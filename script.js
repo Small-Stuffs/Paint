541d6b01453881dc70236670a01d0c1a9a9dae05
Cedrick Garcia <icedgarcia@gmail.com>
2021-08-03 22:42:11 +0800

    filling :heavy_check_mark:

--- script.js
+++ script.js
@@ -2,16 +2,34 @@ const canvas = document.querySelector('#canvas')
 const ctx = canvas.getContext('2d')
 const colors = document.querySelectorAll('.colors')
 const size = document.querySelector('.range')
-
+const mode = document.querySelector('.fill')
 // canvas Size
 canvas.width = 500;
 canvas.height = 500;
 
 ctx.strokeStyle = 'black'
+ctx.fillStyle = 'black'
 ctx.lineWidth = '2.5'
 
 let painting = false
+let filling = false
 
+
+if(mode) {
+  mode.addEventListener('click' , startFilling)
+}
+function startFilling() {
+  if(filling === true) {
+    filling = false
+    mode.innerHTML = 'Fill'
+  }
+  else {
+    filling = true
+    mode.innerHTML = 'Paint'
+    ctx.fillStyle = ctx.strokeStyle
+  }
+
+}
 function startPainting() {
   painting = true
 }
@@ -56,11 +74,18 @@ function changeBrushSize(e) {
 if(size) {
   size.addEventListener('input', changeBrushSize)
 }
+function fillCanvas() {
+  if(filling) {
+    ctx.fillRect(0,0, canvas.height, canvas.width)
+  }
+  
+}
 
 if(canvas) {
   canvas.addEventListener('mousemove', onMouseMove)
   canvas.addEventListener('mousedown', startPainting)
   canvas.addEventListener('mouseup', stopPainting)
   canvas.addEventListener('mouseleave', stopPainting)
+  canvas.addEventListener('click' ,fillCanvas)
 }
 console.log(Array.from(colors))
